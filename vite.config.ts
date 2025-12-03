3d0d4683b799637147a9daa308def62ed4727b75
RenardoAlves <renardo159@hotmail.com>
2025-12-02 22:33:49 -0300

    Deploy inicial

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// Troque "Elder.ly_web" pelo nome EXATO do seu repositório no GitHub
 const repoName = 'Elder.ly_web'
 
 export default defineConfig({
   plugins: [react()],
   base: `/${repoName}/`,
-  server: {
-    open: true,
-  },
-  build: {
-    outDir: 'dist',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
