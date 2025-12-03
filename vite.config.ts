65b1050bfe290257eb3cdd53707b7a188dac47be
RenardoAlves <renardo159@hotmail.com>
2025-12-02 23:04:45 -0300

    Correções de roteamento

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-const repoName = 'Elder.ly_web'
-
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`,
+  base: "/Elder.ly_web/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
